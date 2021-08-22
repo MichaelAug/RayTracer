ffe43f474c87013e83bf735217a4253eef599152
Michael <michael.aug@protonmail.com>
2021-08-22 20:48:44 +0100

    add simple camera, ray and background

--- ray_tracer/src/main.rs
+++ ray_tracer/src/main.rs
@@ -1,28 +1,54 @@
-use cgmath::Vector3;
+use cgmath::{InnerSpace, Point3, Vector3};
+use ray_tracer::Ray;
 
-type Point3 = cgmath::Vector3<f64>;
 type Colour = cgmath::Vector3<f64>;
 
-const IMAGE_WIDTH: i32 = 256;
-const IMAGE_HEIGHT: i32 = 256;
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_WIDTH: i32 = 400;
+const IMAGE_HEIGHT: i32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as i32;
 fn main() {
-    
+    //Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal: Vector3<f64> = Vector3::new(viewport_width,0.0 , 0.0);
+    let vertical: Vector3<f64> = Vector3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vector3::new(0.0, 0.0, focal_length);
 
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
-
+    // Render
     for j in (0..IMAGE_HEIGHT).rev() {
-        eprintln!("Scanlines remaining: {}",j); //eprintln! flushes buffer
+        eprintln!("Scanlines remaining: {}", j); //eprintln! flushes buffer
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH-1) as f64);
-            let g = (j as f64) / ((IMAGE_HEIGHT-1) as f64);
-            let b = 0.25;
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let pixel_col = ray_colour(&r);
 
-            println!("{} {} {}",ir,ig,ib);
+            write_colour(&pixel_col);
         }
     }
     eprintln!("Done");
+}
+
+fn write_colour(pixel_col: &Colour) {
+    println!(
+        "{} {} {}",
+        ((pixel_col.x * 255.999) as i32),
+        ((pixel_col.y * 255.999) as i32),
+        ((pixel_col.z * 255.999) as i32)
+    )
+}
+
+fn ray_colour(r: &Ray) -> Colour {
+    let unit_dir = r.dir.clone().normalize();
+    let t = 0.5 * (unit_dir.y + 1.0);
+    (1.0 - t) * Colour::new(1.0, 1.0, 1.0) + t * Colour::new(0.5, 0.7, 1.0)
 }
